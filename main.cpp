b7d5b68d3ee1e5aa2b33427ddac7545a28ffa143
Elijah Mixon <emixon98@gmail.com>
2026-02-27 23:56:24 -0800

    Basic reasoning/brainstorming for rest of project and documentation

--- main.cpp
+++ main.cpp
@@ -7,6 +7,9 @@ static const int N = 50;
 
 enum classification {BEST, AVG, WORST};
 
+//This works for both Part A and B since it runs off pairs and always 50 integer sized arrays So Part B is knocked out
+//Just make sure to incorporate random and seed gen later for user input, I guess its ok if a user input them as well
+//will just comment out that line of code in case professor runs unit test
 classification arrType(vector<int>& arr){
     int badCases = 0;
     //Check each element and the element to its right, if it is greater than we have an out of order pair
@@ -30,5 +33,14 @@ void insertionSort(){
 }
 
 int main() {
-    
+
 }
+
+//I personally want strict best and worst case scenarios for both clarity and to maximize efficiency when an array
+//is best csae
+
+//Assuming that if every i is < i+1 then the array is ordered, nice and logical
+//I know that under best case insertion sort is O(N), and that selection sor tis always O(N^2)
+//The book details how on average selection sor tis o(n^2/1.8), insertion was defined by prof as O(N^2)
+// based on this conclusion, I would want to use insertion for all best case and selection for everything else
+//But I should look into the logistics of this and ensure I choose the right algo for the job rather than go on hunches
